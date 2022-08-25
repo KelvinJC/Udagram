748d6a358bc75eada44f8773fb497a588ebdfa10
Kelvin JC <ojkelvinchu@gmail.com>
2022-08-25 12:22:33 +0100

    typecast endpoint input

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express from 'express';
+import { Response, Request } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
+
 
 (async () => {
 
@@ -26,7 +28,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //    image_url: URL of a publicly accessible image
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-  app.get("/filteredimage", async (req, res) => {
+  app.get("/filteredimage", async (req: Request, res: Resposne) => {
     let url = req.query.image_url;
 
     if(!url) {
